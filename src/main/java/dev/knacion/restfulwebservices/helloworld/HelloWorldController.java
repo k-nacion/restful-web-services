72e178dbf5ff3035be30b8d1e5227430e53d6516
Kenn <kentot51810@gmail.com>
2021-02-28 11:17:58 +0800

    Added Locale Internationalization. messages as base names seems to be working but not with other custom names.

--- src/main/java/dev/knacion/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/dev/knacion/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,26 +1,39 @@
 package dev.knacion.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 //@RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World Bean");
     }
 
-    @GetMapping("/hello-world-{name}")
-    public String helloWorldName(@PathVariable String name){
-        return "Hello world " + name;
-    }
+//    @GetMapping("/hello-world-{name}")
+//    public String helloWorldName(@PathVariable String name) {
+//        return "Hello world " + name;
+//    }
 
+    @GetMapping("/hello-world-internationalization")
+    public String internationalization(
+            @RequestHeader(required = false, name = "Accept-Language") Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
 }
---
+++ src/main/java/dev/knacion/restfulwebservices/helloworld/config/HelloWorldConfig.java
@@ -0,0 +1,32 @@
+package dev.knacion.restfulwebservices.helloworld.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.spi.LocaleServiceProvider;
+
+@Configuration
+public class HelloWorldConfig {
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource source(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+
+}
