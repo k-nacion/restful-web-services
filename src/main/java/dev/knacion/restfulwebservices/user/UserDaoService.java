f93a39166337890d61e0fc51063c71815eab95f4
Kenn <kentot51810@gmail.com>
2021-02-28 17:14:04 +0800

    Added Swagger for our app.

--- src/main/java/dev/knacion/restfulwebservices/user/UserDaoService.java
+++ src/main/java/dev/knacion/restfulwebservices/user/UserDaoService.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Component
 public class UserDaoService {
 
-    private static List<User> users = new ArrayList<>();
+    private static final List<User> users = new ArrayList<>();
 
     static {
         users.add(new User(1, "Adam", new Date()));
---
+++ src/main/java/dev/knacion/restfulwebservices/user/configuration/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package dev.knacion.restfulwebservices.user.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    //Bean - Docket
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+        //swagger 2
+        //all the paths
+        //all the APIs
+
+}
